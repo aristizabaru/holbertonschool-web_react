5061fc7f57ad7cf2f54cc942d1b40997e8b32d19
aristizabaru <andres.aristizabal@gmail.com>
2022-05-12 18:26:04 +0000

    style: simplified file

--- 0x01-Webpack/task_1/js/dashboard_main.js
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -1,5 +1,5 @@
 import $ from 'jquery';
-import _ from 'lodash';
+const _ = require('lodash');
 
 $.when($.ready).then(function () {
   let counter = 0;
@@ -8,13 +8,11 @@ $.when($.ready).then(function () {
     $('#count').html(`${counter} clicks on the button`);
   }
 
-  $('body').append(`
-    <p>Holberton Dashboard</p>
-    <p>Dashboard data for the students</p>
-    <button>Click here to get started</button>
-    <p id='count'></p>
-    <p>Copyright - Holberton School</p>
-  `);
+  $('body').append('<p>Holberton Dashboard</p>');
+  $('body').append('<p>Dashboard data for the students</p>');
+  $('body').append('<button>Click here to get started</button>');
+  $('body').append($('<p>').attr('id', 'count'));
+  $('body').append('<p>Copyright - Holberton School</p>');
 
   $('button').on('click', _.debounce(updateCounter, 500));
 });
